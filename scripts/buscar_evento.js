f0b53d71dabe3575806022ce92237bca7f1fe831
Dan-1395 <astohuamanepsinodaniela@gmail.com>
2025-09-27 22:24:58 -0500

    commitEvento1

---
+++ scripts/buscar_evento.js
@@ -0,0 +1,15 @@
+function buscarEventos() {
+  const texto = document.getElementById('buscador').value.toLowerCase();
+  
+  document.querySelectorAll('.evento').forEach(evento => {
+    const titulo = evento.querySelector('.nombre-evento').textContent.toLowerCase();
+    if (titulo.includes(texto)) {
+      evento.style.display = "flex"; 
+      evento.classList.toggle("filtro_evento", !titulo.includes(texto));
+    } else {
+      evento.style.display = "none";
+    }
+  });
+}
+document.getElementById('buscador').addEventListener('keyup', buscarEventos);
+document.querySelector('.btn-buscar').addEventListener('click', buscarEventos);
